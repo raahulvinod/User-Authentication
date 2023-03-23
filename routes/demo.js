1ca230c2355d8ad116da3c6b3331b02cdb3cfbe8
Rahul vinod <rahulvinod135@gmail.com>
2023-03-23 11:51:08 +0530

    Storing authentication data in sessions

--- routes/demo.js
+++ routes/demo.js
@@ -79,10 +79,11 @@ router.post('/login', async function (req, res) {
     console.log('Could not log in - Passwords are not match!');
     return res.redirect('/login');
   }
-
-  console.log('User is authenticated!');
-  res.redirect('/admin');
-
+  req.session.user = { id: existingUser._id, email: existingUser.email};
+  req.session.isAuthenticated = true;
+  req.session.save(function () {
+      res.redirect('/admin');
+  })
 });
 
 router.get('/admin', function (req, res) {
